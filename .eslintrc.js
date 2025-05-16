6b971e20951e47c6454eba2c838258f053da97dc
shenxf <shenxf_T_1000@163.com>
2025-05-16 22:04:09 +0800

    init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,11 @@
 module.exports = {
   extends: require.resolve('@umijs/max/eslint'),
+  rules: {
+    "at-rule-no-unknown": [
+      true,
+      {
+        ignoreAtRules: ["tailwind", "apply", "variants", "responsive", "screen"]
+      }
+    ]
+  }
 };
